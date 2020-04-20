a89567d1ab8246a372bb8288b2c2fed2bd0df0f8
Aziz <50262377+azikoDron@users.noreply.github.com>
2020-04-20 13:28:12 +0300

    Add files via upload

--- MyGitController.py
+++ MyGitController.py
@@ -7,7 +7,7 @@ python_files = []
 project_names = []
 
 for i in com3:
-    if ".py" in i:
+    if "." in i:
         python_files.append(i)
     else:
         project_names.append(i)
@@ -27,9 +27,12 @@ for pro in project_names:
     if '{}.txt'.format(pro) not in com4:
         os.system('''CD {0} && git init'''.format(pro))
         initGit = ('IMPORTANT: Initializing git repository: ' + str(pro))
+        with open('{0}\\{1}.txt'.format(pro, pro), 'w') as cr:
+            cr.close()
+
     os.system('''CD {0} && git add .'''.format(pro))
     trackFile = 'Tracked files: ' + str(com4)
-    commit_time = str(time.strftime('%Y%m%d%H%M'))
+    commit_time = str(time.strftime('%Y.%m.%d_%H:%M'))
     my_commit = '''git commit -m "Commit {} "'''.format(commit_time)
     os.system('''CD {0} && {1}'''.format(pro, my_commit))
     commitGit = 'Commitment: {}'.format(my_commit)
@@ -38,6 +41,6 @@ for pro in project_names:
         mf.writelines(initGit + '\n' + workFile + '\n' + trackFile + '\n' + commitGit + '\n')
     print(os.system('CD {} && git diff'.format(pro)))
     os.system('CD {0} && git remote add origin https://github.com/azikoDron/{1}.git && git push -u origin master'.format(pro, pro))
-    print('\n')
+    print('\n') # Fix me
 
 
